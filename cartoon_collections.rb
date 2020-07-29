67be22d1eb06e3c5c5680a963a3302d74e361869
Anney Park <anneypark@gmail.com>
2020-07-29 20:27:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   # Use an Enumerable to square every element in the passed in array
   # Return a new array of the results
-  array.map [|element| element * element]
+  array.map do |element| element * element
 end
 
 def summon_captain_planet(planeteer_calls)
